808bec8a27060dbec83d81ee4e90b40cfd69656b
draffensperger <draff8660@gmail.com>
2015-03-05 06:39:39 -0500

    Updated to use separate golp lib

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # scheduler
 Web service to schedule tasks for Google Sheets add on
+
+# License
+
+MIT Licensed
--- main.go
+++ main.go
@@ -1,60 +1,23 @@
 /*
 Next steps for the go project:
 
-Create a git repository for it
-Encapsulate LP and add a test case for it
 Encapsulate the JSON processing and add a test case for it
 
-
 Set up a Travis CI build that automatically deploys it to Google App Engine
-
 */
 
 // You can edit this code!
 // Click here and start typing.
 package main
 
-// typedef int (*intFunc) ();
-//
-// int
-// bridge_int_func(intFunc f)
-// {
-//		return f();
-// }
-//
-// int fortytwo()
-// {
-//	    return 42;
-// }
-//
-//
-//
-/*
-#cgo CFLAGS: -I./lp_solve
-#cgo LDFLAGS: -L./lp_solve -llpsolve55 -Wl,-rpath=./lp_solve
-#include <stdlib.h>
-#include "lp_lib.h"
-*/
-import "C"
-
 import (
 	"encoding/json"
 	"errors"
 	"fmt"
+	"github.com/draffensperger/golp"
 	"strconv"
 	"time"
-	"unsafe"
 )
-
-/*
-Go REPLs
-http://play.golang.org/
-https://github.com/vito/go-repl
-https://github.com/sbinet/igo
-https://github.com/sriram-srinivasan/gore
-http://labix.org/hsandbox
-
-*/
 
 const SampleJSON = `{	
 	"timeZone": "America/New_York",
@@ -115,8 +78,8 @@ type TimeBlock struct {
 
 type Task struct {
 	Title          string
-	EstimatedHours float32
-	Reward         float32
+	EstimatedHours float64
+	Reward         float64
 	Deadline       time.Time
 	StartOnOrAfter time.Time
 }
@@ -144,9 +107,10 @@ func main() {
 		fmt.Printf(" %v", err)
 	}
 	fmt.Println("Task params:")
-	fmt.Printf("%+v", taskParams)
+	fmt.Printf("%+v\n", taskParams)
 
 	formatLP(taskParams)
+	fmt.Println("")
 }
 
 func formatLP(params TaskParams) {
@@ -154,7 +118,6 @@ func formatLP(params TaskParams) {
 		LP format:
 
 		step 1: break tasks up into hourly chunks, (later on re-weight the longer tasks as less rewarding)
-
 
 		variables are
 			date_hour_taskpart = 1.0 means do taskpart at date and hour
@@ -177,145 +140,67 @@ func formatLP(params TaskParams) {
 	//horizonHours := 22 * 8
 	horizonHours := 8
 
-	ncol := C.int(len(tasks) * horizonHours)
-	lp := C.make_lp(0, ncol)
+	ncol := len(tasks) * horizonHours
+
+	lp := golp.NewLP(0, ncol)
+	//lp.SetVerboseLevel(6)
 
 	for hour := 0; hour < horizonHours; hour++ {
 		for taskNum := 0; taskNum < len(tasks); taskNum++ {
-			nameStr := "h" + strconv.Itoa(hour) + "_t" + strconv.Itoa(taskNum)
-			name := C.CString(nameStr)
-			colNum := hour*len(tasks) + taskNum + 1
-			C.set_col_name(lp, C.int(colNum), name)
-			C.free(unsafe.Pointer(name))
+			colNum := hour*len(tasks) + taskNum
+			lp.SetColName(colNum, "h"+strconv.Itoa(hour)+"_t"+strconv.Itoa(taskNum))
 		}
 	}
 
-	C.set_add_rowmode(lp, 1)
-
-	// Each variable must be between 0 and 1
-
 	// Total tasks done in a hour must be <= 1
 	for hour := 0; hour < horizonHours; hour++ {
-		row := make([]C.double, len(tasks))
-		colNums := make([]C.int, len(tasks))
+		entries := make([]golp.Entry, len(tasks))
 		for taskNum := 0; taskNum < len(tasks); taskNum++ {
-			colNums[taskNum] = C.int(len(tasks)*hour + taskNum + 1)
-			row[taskNum] = 1.0
+			entries[taskNum].Col = len(tasks)*hour + taskNum
+			entries[taskNum].Val = 1.0
 		}
-		C.add_constraintex(lp, C.int(len(tasks)), &row[0], &colNums[0], C.LE, 1.0)
+		lp.AddConstraintSparse(entries, golp.LE, 1.0)
 	}
 
 	// Total amount done on each task must be <= task.EstimatedHours
 	for taskNum, task := range tasks {
-		row := make([]C.double, horizonHours)
-		colNums := make([]C.int, horizonHours)
+		entries := make([]golp.Entry, horizonHours)
 		for hour := 0; hour < horizonHours; hour++ {
-			colNums[hour] = C.int(len(tasks)*hour + taskNum + 1)
-			row[hour] = 1.0
+			entries[hour].Col = len(tasks)*hour + taskNum
+			entries[hour].Val = 1.0
 		}
-		C.add_constraintex(lp, C.int(horizonHours), &row[0], &colNums[0], C.LE, C.double(task.EstimatedHours))
+		lp.AddConstraintSparse(entries, golp.LE, task.EstimatedHours)
 	}
-	C.set_add_rowmode(lp, 0)
 
 	// Objective function
-	decayRate := float32(0.95)
-	curHourValue := float32(1.0)
-	row := make([]C.double, len(tasks)*horizonHours+1)
+	decayRate := 0.95
+	curHourValue := 1.0
+	row := make([]float64, len(tasks)*horizonHours)
 	for hour := 0; hour < horizonHours; hour++ {
 		for taskNum, task := range tasks {
-			row[len(tasks)*hour+taskNum+1] = C.double(curHourValue * task.Reward / task.EstimatedHours)
+			row[len(tasks)*hour+taskNum] = curHourValue * task.Reward / task.EstimatedHours
 		}
 		curHourValue *= decayRate
 	}
 
-	C.set_obj_fn(lp, &row[0])
-
-	C.set_maxim(lp)
+	lp.SetObjFn(row, true)
 
 	fmt.Println("\n")
 	fmt.Println("LP formulation:")
-	C.write_LP(lp, C.stdout)
+	lp.WriteToStdout()
 
-	ret := C.solve(lp)
+	ret := lp.Solve()
 	fmt.Printf("Solve returned: %v\n", ret)
 
-	obj := C.get_objective(lp)
+	obj := lp.GetObjective()
 	fmt.Printf("Objective value: %v\n", obj)
 
-	C.get_variables(lp, &row[0])
+	vars := lp.GetVariables()
 	for hour := 0; hour < horizonHours; hour++ {
 		for taskNum := 0; taskNum < len(tasks); taskNum++ {
 			nameStr := "h" + strconv.Itoa(hour) + "_t" + strconv.Itoa(taskNum)
-			val := row[len(tasks)*hour+taskNum]
+			val := vars[len(tasks)*hour+taskNum]
 			fmt.Printf("%v: %v\n", nameStr, val)
 		}
 	}
-
-	C.delete_lp(lp)
-}
-
-func solve() {
-	fmt.Println("Hello, 世界")
-	Seed(44)
-	fmt.Println(Random())
-	fmt.Println(Random())
-	fmt.Println(Random())
-
-	var ncol C.int
-	ncol = 2
-	lp := C.make_lp(0, ncol)
-	C.set_col_name(lp, 1, C.CString("x"))
-	C.set_col_name(lp, 2, C.CString("y"))
-
-	C.set_add_rowmode(lp, 1)
-
-	colno := []C.int{1, 2}
-
-	var j C.int
-	j = 2
-
-	row := []C.double{120.0, 210.0}
-	C.add_constraintex(lp, j, &row[0], &colno[0], C.LE, 15000)
-
-	row = []C.double{110.0, 30.0}
-	C.add_constraintex(lp, j, &row[0], &colno[0], C.LE, 4000)
-
-	row = []C.double{1.0, 1.0}
-	C.add_constraintex(lp, j, &row[0], &colno[0], C.LE, 75)
-
-	C.set_add_rowmode(lp, 0)
-
-	row = []C.double{143.0, 60.0}
-	C.set_obj_fnex(lp, j, &row[0], &colno[0])
-	C.set_maxim(lp)
-	C.write_LP(lp, C.stdout)
-	C.set_verbose(lp, C.IMPORTANT)
-
-	ret := C.solve(lp)
-	fmt.Println("Solve returned: ")
-	fmt.Println(ret)
-
-	obj := C.get_objective(lp)
-	fmt.Println("Objective value: ")
-	fmt.Println(obj)
-
-	C.get_variables(lp, &row[0])
-	fmt.Println("x: ")
-	fmt.Println(row[0])
-
-	fmt.Println("y: ")
-	fmt.Println(row[1])
-
-	C.delete_lp(lp)
-
-	f := C.intFunc(C.fortytwo)
-	fmt.Println(int(C.bridge_int_func(f)))
-}
-
-func Random() int {
-	return int(C.random())
-}
-
-func Seed(i int) {
-	C.srandom(C.uint(i))
 }
